c3270b1c414d8a77c7716db0304993466ebe0a66
IsasiaJ <isasia.j@gmail.com>
2024-08-11 07:47:13 +0200

    Se añaden en engro las lineas sin flujo de potencia. Se pone el rango de colores fijo entre 0 y 1. (verde a rojo)

--- Plotter.py
+++ Plotter.py
@@ -38,11 +38,8 @@ fig, ax = plt.subplots(figsize=(12, 10))  # Tamaño de figura ajustado
 fig.patch.set_facecolor('white')  # Fondo blanco para la figura
 ax.set_facecolor('lightgrey')  # Fondo gris claro para el área de gráficos
 
-# Obtener los valores de los pesos de las aristas
-weights = nx.get_edge_attributes(G, 'weight').values()
-
-# Normalizar los pesos para que estén en el rango [0, 1]
-norm = plt.Normalize(vmin=min(weights), vmax=max(weights))
+# Fijar la normalización de los pesos en el rango [0, 1]
+norm = plt.Normalize(vmin=0, vmax=1)
 
 # Crear un mapa de colores (colormap) invertido de verde a rojo
 cmap = plt.cm.RdYlGn_r
@@ -50,19 +47,18 @@ cmap = plt.cm.RdYlGn_r
 # Elegir el diseño para el grafo
 pos = nx.spring_layout(G, k=5, iterations=1000, seed=42)  # Ajusta 'k' y 'iterations' para mejorar la disposición
 
-# Alternativas: Puedes probar con otros layouts si el diseño spring no es óptimo
-# pos = nx.circular_layout(G)
-# pos = nx.kamada_kaway_layout(G)
-# pos = nx.shell_layout(G)
-
 # Dibujar nodos
 nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=500, ax=ax)
 
 # Dibujar aristas con colores basados en los pesos
 for (u, v, d) in G.edges(data=True):
     weight = d['weight']
+    if weight == 0:
+        edge_color = 'black'
+    else:
+        edge_color = cmap(norm(weight))
     # Dibujar líneas con un contorno negro para mejorar el contraste
-    nx.draw_networkx_edges(G, pos, edgelist=[(u, v)], width=3, alpha=0.8, edge_color=[cmap(norm(weight))], ax=ax, style='solid')
+    nx.draw_networkx_edges(G, pos, edgelist=[(u, v)], width=3, alpha=0.8, edge_color=[edge_color], ax=ax, style='solid')
 
 # Añadir etiquetas a los nodos que incluyen el valor extra
 labels = {node: f"{int(node)}\n{data['LMP']} €/MWh" for node, data in G.nodes(data=True)}
