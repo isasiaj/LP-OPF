bba82a78b8a0a81a37a46d71e4439de82aa5e08d
IsasiaJ <isasia.j@gmail.com>
2024-12-07 17:49:49 +0100

    Se comenta PowerModels y PowerPlots.

--- Funciones/gestorResultados.jl
+++ Funciones/gestorResultados.jl
@@ -109,11 +109,11 @@ function gestorResultados(modelo, solGeneradores, solFlujos, solAngulos, solLMP,
 
         end
 
-        # Se imprime la solución obtenida en caso de utilizar el paquete PowerModels.jl
-        # En caso que exista el archivo .m
-        if solucion != 0
-            print("\nCoste final obtenido en PowerModels: ", round(solucion["objective"], digits = 2), "€/h")
-        end
+        # # Se imprime la solución obtenida en caso de utilizar el paquete PowerModels.jl
+        # # En caso que exista el archivo .m
+        # if solucion != 0
+        #     print("\nCoste final obtenido en PowerModels: ", round(solucion["objective"], digits = 2), "€/h")
+        # end
 
         # Imprime en pantalla el coste final que se obtiene tras la optimización
         println("\nCoste final con el programa: ", round(objective_value(modelo), digits = 2), " €/h")
